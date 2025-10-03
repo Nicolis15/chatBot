f6cf3e36a71e5b4d1617a79ba0549aea30e4e20b
Nicolis15 <Nicolistrabajo@gmail.com>
2025-10-03 10:49:51 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+export default function App() {
+  return (
+    <div class="bg-amber-600 tex">
+    Hola Minombre es Nicoalas Lis Cruz
+    </div>
+  )
+}
+
+
